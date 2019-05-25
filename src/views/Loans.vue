4ef7431b13116d4c323f631e9c503e22ccf0d4ae
JohnTrump <wujunchuan2008@gmail.com>
2019-05-25 14:50:06 +0800

    租赁记录

--- src/views/Loans.vue
+++ src/views/Loans.vue
@@ -1,21 +1,35 @@
 <template>
   <div class="loans">
-    <h1>Loans</h1>
     <template v-for="(loan, index) in loansList">
       <a :key="index" @click="navto(loan)">
-        <div>
-          <span>{{ loan.type }} : {{ loan.total_staked }}</span>
-          <span>租金：{{ loan.payment }}</span>
-        </div>
-        <div>
-          <span>接收者: {{ loan.payment }}</span>
-          <span>续租池: {{ loan.balance }}</span>
-          <span
-            >续租时间：
-            {{
-              loan.expiration | formatTime({ format: "YY-MM-DD HH:mm" })
-            }}</span
-          >
+        <div class="card">
+          <div class="left">
+            <div class="part_1">
+              <div class="total_staked">
+                <h1>{{ loan.type }}</h1>
+                <div class="amount">{{ loan.total_staked }}</div>
+              </div>
+              <div class="payment">
+                <h1>租金</h1>
+                <div class="amount">{{ loan.payment }}</div>
+              </div>
+            </div>
+            <div class="part_2">
+              <div class="wrapper">
+                <div class="detail">
+                  续租时间:
+                  {{ loan.expiration | formatTime({ format: "YYYY/MM/DD" }) }}
+                </div>
+                <div class="detail">接收者：{{ loan.receiver }}</div>
+              </div>
+              <div class="wrapper">
+                <div class="detail">续租池：{{ loan.balance }}</div>
+              </div>
+            </div>
+          </div>
+          <div class="right">
+            <img src="../assets/icon-nav-grey.png" />
+          </div>
         </div>
       </a>
     </template>
@@ -58,6 +72,60 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.loans {
+.card {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0 15px;
+  background: #ffffff;
+  box-shadow: inset 0 0 0 0 #e8e8e8, inset 0 0 0 0 #e8e8e8;
+  margin-bottom: 10px;
+  border-top: 1px solid #e8e8e8;
+  border-bottom: 1px solid #e8e8e8;
+  .left {
+    width: 94%;
+    .part_1 {
+      display: flex;
+      align-items: center;
+      border-bottom: 1px solid #e8e8e8;
+      box-shadow: inset 0 0 0 0 #e8e8e8;
+      min-height: 63px;
+      .amount {
+        font-family: "MarkPro-Medium";
+        font-size: 16px;
+        color: #323232;
+      }
+      h1 {
+        font-size: 12px;
+        color: #888888;
+        line-height: 16px;
+      }
+      .total_staked,
+      .peyment {
+        width: 50%;
+      }
+    }
+    .part_2 {
+      font-size: 12px;
+      color: #323232;
+      line-height: 18px;
+      padding: 10px 0 15px;
+      display: flex;
+      // align-items: center;
+      .wrapper {
+        width: 50%;
+      }
+      .detail {
+        display: inline-block;
+      }
+    }
+  }
+  // 箭头
+  .right {
+    display: flex;
+    align-items: center;
+    position: relative;
+    left: 17px;
+  }
 }
 </style>
