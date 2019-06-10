98f29cedc64ba894e206abe7173202c47a093fef
JohnTrump <wujunchuan2008@gmail.com>
2019-06-10 21:13:44 +0800

    修改节点地址

--- src/config/network.js
+++ src/config/network.js
@@ -3,14 +3,15 @@
  * @Author: JohnTrump
  * @Date: 2019-04-19 16:24:44
  * @Last Modified by: JohnTrump
- * @Last Modified time: 2019-06-10 19:19:45
+ * @Last Modified time: 2019-06-10 21:10:21
  */
 export default {
   blockchain: "eos",
   protocol: "https",
   // host: "api.eosbeijing.one",
   // TODO: for debug
-  host: "mainnet.meet.one",
+  host: "mainnet-tw.shareeos.com",
+  // host: "mainnet.meet.one",
   // host: "mainnet.shareeos.com",
   port: 443,
   chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906"
