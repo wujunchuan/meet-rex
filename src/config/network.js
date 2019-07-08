bc9356e25d116ebac60ace0ddeb108ac064dd66a
JohnTrump <wujunchuan2008@gmail.com>
2019-07-08 15:38:44 +0800

    替换基于Geo的代理

--- src/config/network.js
+++ src/config/network.js
@@ -9,9 +9,10 @@ export default {
   blockchain: "eos",
   protocol: "https",
   // host: "api.eosbeijing.one",
-  host: "mainnet-tw.shareeos.com",
+  //host: "mainnet-tw.shareeos.com",
   // host: "mainnet.meet.one",
   // host: "mainnet.shareeos.com",
+  host: "public.eosinfra.io",
   port: 443,
   chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906"
 };
