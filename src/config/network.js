597d591abaee6974ef3507ea47d7610c0a625038
JohnTrump <wujunchuan2008@gmail.com>
2019-12-16 15:12:42 +0800

    Change Host

--- src/config/network.js
+++ src/config/network.js
@@ -3,14 +3,14 @@
  * @Author: JohnTrump
  * @Date: 2019-04-19 16:24:44
  * @Last Modified by: JohnTrump
- * @Last Modified time: 2019-12-16 14:06:38
+ * @Last Modified time: 2019-12-16 15:12:36
  */
 export default {
   blockchain: "eos",
   protocol: "https",
-  // host: "mainnet.shareeos.com",
+  host: "mainnet.shareeos.com",
   // //TODO: 切换成 mainnet.shareeos.com [cdn speed+]
-  host: "mainnet.meet.one",
+  // host: "mainnet.meet.one",
   port: 443,
   chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906"
 };
