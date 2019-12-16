90f646e4a3ca58e7a56e9744ddb914b964f8dd5b
JohnTrump <wujunchuan2008@gmail.com>
2019-12-16 15:24:03 +0800

    extract .env variable

--- src/config/network.js
+++ src/config/network.js
@@ -3,14 +3,12 @@
  * @Author: JohnTrump
  * @Date: 2019-04-19 16:24:44
  * @Last Modified by: JohnTrump
- * @Last Modified time: 2019-12-16 15:12:36
+ * @Last Modified time: 2019-12-16 15:20:10
  */
 export default {
   blockchain: "eos",
-  protocol: "https",
-  host: "mainnet.shareeos.com",
-  // //TODO: 切换成 mainnet.shareeos.com [cdn speed+]
-  // host: "mainnet.meet.one",
-  port: 443,
-  chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906"
+  protocol: process.env.VUE_APP_RPC_PROTOCOL,
+  host: process.env.VUE_APP_RPC_HOST,
+  port: process.env.VUE_APP_RPC_PORT,
+  chainId: process.env.VUE_APP_RPC_ID
 };
